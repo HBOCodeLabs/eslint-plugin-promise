4cefb8db61153e275f9fd9d0772842423da39482
Brendan Clark <brendan.clark@hbo.com>
2017-01-20 19:08:10 -0800

    Generalize options for param-names

--- rules/param-names.js
+++ rules/param-names.js
@@ -2,21 +2,25 @@ module.exports = {
   create: function (context) {
     return {
       NewExpression: function (node) {
+
         if (node.callee.name === 'Promise' && node.arguments.length === 1) {
           var params = node.arguments[0].params
 
           if (!params || !params.length) { return }
 
-          if (params[0].name !== 'resolve') {
-            return context.report(node, 'Promise constructor\'s first parameter must be named resolve')
-          }
-
-          if (params[1] && params[1].name !== 'reject') {
-            return context.report(node, 'Promise constructor\'s second parameter must be named reject')
-          }
-
-          if (params[2] && params[2].name !== 'onCancel') {
-            return context.report(node, 'Promise constructor\'s third parameter must be named onCancel')
+          var defaults = ['resolve', 'reject']
+          for (var i = 0; i < params.length; i++) {
+            var actualName = params[i].name;
+            var expectedName = context.options[i] || defaults[i];
+            if (!expectedName || !expectedName.length) {
+              return context.report(node, 'Promise constructor has unexpected parameter ' + i + ' "' + actualName + '"')
+            }
+            if (typeof expectedName === 'string') {
+              expectedName = [expectedName]
+            }
+            if (expectedName.indexOf(actualName) === -1) {
+              return context.report(node, 'Promise constructor parameter ' + i + ' "' + actualName + '" must be named: ' + expectedName.join(", "))
+            }
           }
         }
       }
--- test/param-names.test.js
+++ test/param-names.test.js
@@ -6,23 +6,37 @@ var RuleTester = require('eslint').RuleTester
 var ruleTester = new RuleTester()
 ruleTester.run('param-names', rule, {
   valid: [
+    // defaults
     'new Promise(function(resolve, reject) { })',
     'new Promise(function(resolve) { })',
-    'new Promise(function(resolve, reject, onCancel) { })'
+
+    // options
+    { code: 'new Promise(function(resolve, reject) { })', options: ['resolve', 'reject']},
+    { code: 'new Promise(function(fulfill, reject, cancel) { })', options: [['resolve', 'fulfill'], 'reject', ['onCancel', 'cancel']]},
   ],
 
   invalid: [
     {
       code: 'new Promise(function(reject, resolve) { })',
-      errors: [ { message: 'Promise constructor\'s first parameter must be named resolve' } ]
+      errors: [ { message: 'Promise constructor parameter 0 "reject" must be named: resolve' } ]
     },
     {
       code: 'new Promise(function(resolve, rej) { })',
-      errors: [ { message: 'Promise constructor\'s second parameter must be named reject' } ]
+      errors: [ { message: 'Promise constructor parameter 1 "rej" must be named: reject' } ]
     },
     {
       code: 'new Promise(function(resolve, reject, cancelled) { })',
-      errors: [ { message: 'Promise constructor\'s third parameter must be named onCancel' } ]
+      errors: [ { message: 'Promise constructor has unexpected parameter 2 "cancelled"' } ]
+    },
+    {
+      code: 'new Promise(function(resolve, reject) { })',
+      options: ['fulfill', 'throw'],
+      errors: [ { message: 'Promise constructor parameter 0 "resolve" must be named: fulfill'  } ]
+    },
+    {
+      code: 'new Promise(function(reject, resolve) { })',
+      options: [['resolve', 'fulfill'], 'reject'],
+      errors: [ { message: 'Promise constructor parameter 0 "reject" must be named: resolve, fulfill'  } ]
     }
   ]
 })
