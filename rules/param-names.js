d5d746801a02742fc5a28fdd806ac0d0130ca643
Brendan Clark <brendan.clark@hbo.com>
2017-01-20 16:58:23 -0800

    param-names validates Bluebird's onCancel constructor argument

--- rules/param-names.js
+++ rules/param-names.js
@@ -8,11 +8,15 @@ module.exports = {
           if (!params || !params.length) { return }
 
           if (params[0].name !== 'resolve') {
-            return context.report(node, 'Promise constructor parameters must be named resolve, reject')
+            return context.report(node, 'Promise constructor\'s first parameter must be named resolve')
           }
 
           if (params[1] && params[1].name !== 'reject') {
-            return context.report(node, 'Promise constructor parameters must be named resolve, reject')
+            return context.report(node, 'Promise constructor\'s second parameter must be named reject')
+          }
+
+          if (params[2] && params[2].name !== 'onCancel') {
+            return context.report(node, 'Promise constructor\'s third parameter must be named onCancel')
           }
         }
       }
--- test/param-names.test.js
+++ test/param-names.test.js
@@ -7,17 +7,22 @@ var ruleTester = new RuleTester()
 ruleTester.run('param-names', rule, {
   valid: [
     'new Promise(function(resolve, reject) { })',
-    'new Promise(function(resolve) { })'
+    'new Promise(function(resolve) { })',
+    'new Promise(function(resolve, reject, onCancel) { })'
   ],
 
   invalid: [
     {
       code: 'new Promise(function(reject, resolve) { })',
-      errors: [ { message: 'Promise constructor parameters must be named resolve, reject' } ]
+      errors: [ { message: 'Promise constructor\'s first parameter must be named resolve' } ]
     },
     {
       code: 'new Promise(function(resolve, rej) { })',
-      errors: [ { message: 'Promise constructor parameters must be named resolve, reject' } ]
+      errors: [ { message: 'Promise constructor\'s second parameter must be named reject' } ]
+    },
+    {
+      code: 'new Promise(function(resolve, reject, cancelled) { })',
+      errors: [ { message: 'Promise constructor\'s third parameter must be named onCancel' } ]
     }
   ]
 })
